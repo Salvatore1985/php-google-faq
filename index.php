0b112587c2b3ffdf60802bf2ee22eed1308958a1
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-04-21 17:53:57 +0200

    list

--- index.php
+++ index.php
@@ -49,37 +49,49 @@ $implementing = [
     [
         'request' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
 
-        'answer' => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. 
+        'answer' => '<section class="mb-4">Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. 
                         Per noi è imperativo offrire un\'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro 
-                        e accessibili quando ne hai bisogno.
+                        e accessibili quando ne hai bisogno.</section>
 
-                        Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua 
+                        <section class="mb-4">Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua 
                         privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia 
                         di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale 
                         in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre 
                         sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, 
                         la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni 
-                        che condividi con Google, hai il massimo controllo.
+                        che condividi con Google, hai il massimo controllo.</section>
                         
-                        Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua 
-                        famiglia online, nel Centro Google per la sicurezza online.
+                        <section class="mb-4">Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua 
+                        famiglia online, nel <a href="#" class="text-decoration-none">Centro Google per la sicurezza online.</a></section>
                         
-                        Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, 
-                        lasciando a te il controllo.'
+                        <section class="mb-4"><a href="#" class="text-decoration-none">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, 
+                        lasciando a te il controllo.</section>'
     ],
     [
         'request' => 'Perché il mio account è associato a un paese?',
 
-        'answer' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+        'answer' => '  <section class="mb-4">Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</section>
 
-                        La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto 
-                        delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una 
-                        delle due società seguenti:
-                            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione 
-                            europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
-                            Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-                        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-                    
+                       <ol>
+                            <li> 
+                            La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. 
+                            Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                                        <ul class"my-lower-alpha">
+                                            <li> 
+                                                Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, 
+                                                Liechtenstein e Norvegia) o in Svizzera.
+                                            </li> 
+                                                Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                                            <li> 
+
+                                            </li> 
+                                        </ul>
+                            </li> 
+                                          
+                            <li>
+                                 La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                            </li>
+                        </ol>
                     Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che 
                     li offre o dal paese a cui è associato il tuo account.'
     ],
