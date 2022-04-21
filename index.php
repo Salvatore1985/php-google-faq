f9cd0576f8dc603eb1be58bea037e3c1736f0c9a
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-04-21 17:37:22 +0200

    add link

--- index.php
+++ index.php
@@ -18,16 +18,16 @@ $implementing = [
                     al diritto all\'oblio?', 'Come fa Google a proteggere la mia privacy e a tenere le mie 
                     informazioni al sicuro?',
 
-        'answer' => 'La recente decisione della Corte di giustizia dell\'Unione europea ha profonde conseguenze per 
+        'answer' => '<section class="mb-4">La recente <a href="#" class="text-decoration-none">decisione della Corte di giustizia dell\'Unione europea</a> ha profonde conseguenze per 
                     i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori 
                     di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per 
-                    poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+                    poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi. </section>
 
-                    Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. 
+                    <section class="mb-4">Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. 
                     Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il 
-                    diritto dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
+                    diritto dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</section>
                     
-                    Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la 
+                    <section class="mb-4"> Per presentare una richiesta di rimozione, compila questo <a href="#" class="text-decoration-none"> modulo web</a>. Riceverai una risposta automatica che conferma la 
                     ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po\' di tempo 
                     perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono 
                     informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le 
@@ -35,16 +35,16 @@ $implementing = [
                     professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). 
                     Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per 
                     prendere decisioni in merito al tuo caso. Se non sei d\'accordo con la nostra valutazione, puoi rivolgerti all\'Autorità 
-                    garante per la protezione dei dati personali nel tuo paese.
+                    garante per la protezione dei dati personali nel tuo paese.</section>
                     
-                    Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per 
+                    <section class="mb-4">Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per 
                     perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di 
                     ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della 
-                    Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
+                    Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</section>
                     
-                    Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
+                    <section class="mb-4">Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
                     modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei 
-                    quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.'
+                    quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</section>'
     ],
     [
         'request' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
@@ -179,8 +179,8 @@ $implementing = [
     </header>
     <main>
         <div class="container">
-            <div class="row py-5">
-                <div class="col-8 offset-2">
+            <div class="row py-5 justify-content-center">
+                <div class="col-10 ">
                     <?php foreach ($implementing  as $key => $value) { ?>
                         <h4 class="py-3">
                             <strong><?php echo  $value['request'] ?></strong>
